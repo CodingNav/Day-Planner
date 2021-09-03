3e0af53688349a9135765b670e7cc0f27b26d15c
CodingNav <navleenkaurr@gmail.com>
2021-09-02 23:44:27 -0400

    Added comments for click event function

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,14 +6,14 @@ currentDateEl.textContent = currentDate;
 
 // click event listener for save button
 $(".saveBtn").on("click", function() {
+    // finds value of user input on the specific save button row
     var textarea = $(this).parent().find("textarea");
     var text = textarea.val();
 
-    console.log(textarea);
-    console.log(text);
-
+    // finds time on the specific save button row
     var timeTag = $(this).parent().find(".hour");
     var time = timeTag.data("time");
 
+    // saves time and text to local storage
     localStorage.setItem(time, text);
 });
